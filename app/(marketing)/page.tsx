04da9807ba2c926db2a243687c0024f4b2d83052
Saurabh <saurabhhumane125@gmail.com>
2026-08-20 12:16:42 +0530

    chore: remove ribbon overlay

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,12 +5,10 @@ import { Portfolio } from "@/components/sections/Portfolio"
 import { WhyVisionCraft } from "@/components/sections/WhyVisionCraft"
 import { Testimonials } from "@/components/sections/Testimonials"
 import { FinalCTA } from "@/components/sections/FinalCTA"
-import { OpeningRibbon } from "@/components/ui/OpeningRibbon"
 
 export default function Home() {
   return (
     <>
-      <OpeningRibbon />
       <Hero />
       <TrustedMarquee />
       <Services />
